674715e33a4063ddcef2d0ecae5a59ffc02483b9
Alex Conrad <amconrad96@icloud.com>
2022-06-25 11:59:55 -0500

    HW3 Stuck

---
+++ Section3Hw.js
@@ -0,0 +1,28 @@
+// 1
+const pizzaToppings = ["Pepperoni", "Sausage", "Bacon", "Ham"];
+// 2
+function greetingCustomer() {
+console.log("Welcome to AlexPizza, Our toppings are:")
+}
+greetingCustomer();
+
+for (let topping of pizzaToppings) {
+console.log(topping)
+}
+
+// 3
+function getPizzaOrder(large, thick, ...toppings) {
+const toppings = ["Pepperoni", "Sausage", "Bacon", "Ham"];
+
+}
+const order = getPizzaOrder
+console.log(order);
+
+getPizzaOrder();
+
+// 4
+function preparePizza()
+
+
+// 5
+function servePizza()
