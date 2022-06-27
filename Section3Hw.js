a3a83baffc741448e3a32f9cc31dbabec3600af3
Alex Conrad <amconrad96@icloud.com>
2022-06-26 22:26:45 -0500

    HW3 in progress

--- Section3Hw.js
+++ Section3Hw.js
@@ -2,7 +2,7 @@
 const pizzaToppings = ["Pepperoni", "Sausage", "Bacon", "Ham"];
 // 2
 function greetingCustomer() {
-console.log("Welcome to AlexPizza, Our toppings are:")
+console.log("Welcome to AlexPizza, Our toppings are:");
 }
 greetingCustomer();
 
@@ -11,18 +11,61 @@ console.log(topping)
 }
 
 // 3
-function getPizzaOrder(large, thick, ...toppings) {
-const toppings = ["Pepperoni", "Sausage", "Bacon", "Ham"];
-
+function getPizzaOrder(size, crust, ...topping){
+  console.log("One " + size + crust + "crust pizza with " + topping + "coming up! ");
+let order = [size, crust, topping]
+return order;
 }
-const order = getPizzaOrder
-console.log(order);
 
-getPizzaOrder();
+getPizzaOrder("medium", "thick", "Pepperoni", "Sausage", "Bacon", "Ham");
+
+for (let order of getPizzaOrder)
+console.log(getPizzaOrder);
 
 // 4
-function preparePizza()
+function preparePizza(){
 
 
+}
+
+preparePizza();
 // 5
 function servePizza()
+
+
+// 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function example(param1, param2, ...rest){
+let array = [param1, param2, rest]
+return array;
+
+}
+
+example('Long', 'Short', 'fat', 'dog', 'cat');
+
+
+function examObj(param1, param2, ...param3){
+let object = {
+  ObjA: param1[0],
+  ObjB: param2[1],
+  objC: param3[2],
+};
+return console.log `I found a cool ${object.objB}`;
+}
