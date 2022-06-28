ba5f14e3afccb9cce3c1d2587c81046531126a5a
Alex Conrad <amconrad96@icloud.com>
2022-06-28 12:26:19 -0500

    HW3 update

--- Section3Hw.js
+++ Section3Hw.js
@@ -7,7 +7,7 @@ greetingCustomer();
 
 for (let topping of pizzaToppings) {
 console.log(topping)
-}
+};
 
 // 3
 function getPizzaOrder(size, crust, ...topping){
@@ -18,4 +18,16 @@ function getPizzaOrder(size, crust, ...topping){
 getPizzaOrder("medium ", "thick","Pepperoni"," Sausage"," Bacon"," Ham",'');
 
 
+return getPizzaOrder()
 
+
+//4
+function preparePizza(medium, thick, ...topping) {
+let Pizza = {
+  size: medium [0]
+  crust: thick [1]
+  toppings: topping [2]
+  };
+  return console.log(Pizza);
+  }
+function preparePizza(Pizza ("medium", "thick", "Pepperoni", "Sausage", "Bacon", "Ham"));
